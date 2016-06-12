28dfa4ea9a6803cb33066d830d80fdac79686492
Hubert Ray <hubert.ray@gumi-europe.com>
2016-06-12 22:07:54 +0200

    Adds a search keyword option

--- rfc/rfc.py
+++ rfc/rfc.py
@@ -13,18 +13,82 @@ __version__ = "0.5"
 
 class Config(object):
     LOCAL_STORAGE_PATH = "~/.rfc"
+    INDEX_NAME = "rfc-index.txt"
+
+
+class RFCIndexReader(object):
+    START_LINE_REGEX = re.compile("^[0-9]+")
+
+    def __init__(self):
+        super().__init__()
+        self._path = os.path.join(os.path.expanduser(Config.LOCAL_STORAGE_PATH), Config.INDEX_NAME)
+        self.kb = set()
+        self._parse_index()
+
+    def _parse_index(self):
+        docstring = None
+        with open(self._path) as opened_file:
+            for line in opened_file:
+                if self._is_start_line(line):
+                    docstring = line
+
+                elif self._is_end_line(line) and docstring:
+                    self.kb.add(RFCDocument(docstring))
+                    docstring = None
+
+                elif self._is_content_line(line) and docstring:
+                    docstring += line
+
+    def _is_start_line(self, line):
+        return self.START_LINE_REGEX.match(line)
+
+    @staticmethod
+    def _is_end_line(line):
+        return len(line) <= 2  # CR, LF, or both
+
+    def _is_content_line(self, line):
+        return not self._is_end_line(line) and not self._is_start_line(line)
+
+    def find(self, keyword):
+        return [doc for doc in self.kb if doc.contains(keyword)]
+
+
+class RFCDocument(object):
+    ID_REGEX = re.compile("^[0-9]+")
+
+    def __init__(self, index_string):
+        super().__init__()
+        self.desc = index_string
+        self.id = self._parse_id()
+
+    def _parse_id(self):
+        return self.ID_REGEX.findall(self.desc)[0]
+
+    def contains(self, search_string):
+        return search_string in self.desc
+
+    def __str__(self):
+        return self.desc
 
 
 class RFCDownloader(object):
     RFC_HOME = "http://www.rfc-editor.org"
     RFC_BULK = "https://www.rfc-editor.org/in-notes/tar/RFC-all.tar.gz"
+    RFC_INDEX = "https://www.ietf.org/download/rfc-index.txt"
 
-    def update_bulk(self):
+    def update(self):
         full_path = self._get_storage_path()
         shutil.rmtree(full_path)
         os.mkdir(full_path)
+        self._update_bulk(full_path)
+        self._update_index(full_path)
+
+    def _update_bulk(self, full_path):
         self._download_file(self.RFC_BULK, full_path)
         self._uncompress_bulk_file()
+
+    def _update_index(self, full_path):
+        self._download_file(self.RFC_INDEX, full_path)
 
     def is_connected(self):
         try:
@@ -46,7 +110,7 @@ class RFCDownloader(object):
         u = urlopen(url)
         f = open(os.path.join(dest, file_name), 'wb')
         meta = u.info()
-        file_size = 0
+        file_size = 1
         for header in meta._headers:
             if header[0] == "Content-Length":
                 file_size = int(header[1])
@@ -93,27 +157,26 @@ class RFCSearcher(object):
         self._path = os.path.expanduser(scan_path)
         if not os.path.exists(self._path):
             os.mkdir(self._path)
-        self._known_documents = set()
+        self._known_documents_ids = set()
         self._scan()
         self._pager = self._get_pager(pager)
-        if len(self._known_documents) == 0:
+        if len(self._known_documents_ids) == 0:
             raise NoRFCFound()
 
     def is_available(self, rfc_number):
-        return rfc_number in self._known_documents
+        return rfc_number in self._known_documents_ids
 
     def open(self, rfc_number):
         if not self.is_available(rfc_number):
             raise RFCNotFoundException("RFC %d not found" % rfc_number)
         os.path.join(self._path)
-        print(self._pager)
         os.system("{} {}".format(self._pager, self._get_file_path(rfc_number)))
 
     def _scan(self):
         for file_name in os.listdir(self._path):
             if self.FILE_REGEX.match(file_name):
                 for num in self.NUM_REGEX.findall(file_name):
-                    self._known_documents.add(int(num))
+                    self._known_documents_ids.add(int(num))
 
     def _get_file_path(self, rfc_number):
         return os.path.join(self._path, "rfc%d.txt" % rfc_number)
@@ -135,7 +198,7 @@ def update_docs():
     if not downloader.is_connected():
         print("No internet connection to update, exiting...", file=sys.stderr)
         exit(-1)
-    downloader.update_bulk()
+    downloader.update()
 
 
 def main():
@@ -147,19 +210,32 @@ def main():
         epilog="Released under GPLv3")
     parser.add_argument("--update", required=False, default=False, action='store_true',
                         help="Updates the local copy of RFC documents with the latest (weekly) publication of the IETF")
-    parser.add_argument(metavar='RFC_NUMBER', type=int, nargs=1, dest="RFC_NUMBER",
+
+    parser.add_argument(metavar='RFC_NUMBER', type=int, nargs="?", dest="RFC_NUMBER",
                         help="Opens the RFC_NUMBER for reading")
+
     parser.add_argument("--pager", "-p", nargs=1, dest="pager", required=False, default=None,
                         help="Uses the given program to open RFC documents. "
                              "Default program is env var $PAGER or `less` if not found")
+
+    parser.add_argument("-k", "--keyword", dest="keyword",
+                        help="Prints the summary of all known RFC documents matching the keyword and exits")
+
     parser.add_argument('--version', action='version',
                         version='%(prog)s {version}'.format(version=__version__))
 
     config = parser.parse_args()
+
     pager = config.pager[0] if config.pager else None
     number = config.RFC_NUMBER[0]
     if config.update:
         update_docs()
+
+    if config.keyword:  # TODO: An app object so we can handle stuff when no db in place
+        index = RFCIndexReader()
+        for rfcdoc in index.find(config.keyword):
+            print(rfcdoc)
+        exit(0)
 
     try:
         reader = RFCSearcher(pager=pager)
