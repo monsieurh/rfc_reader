537b709dbdaa605a6e8cf58235f3cf6e18778143
Hubert Ray <hubert.ray@gumi-europe.com>
2016-06-12 13:23:28 +0200

    working RFC Downloader

--- rfc/rfc.py
+++ rfc/rfc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+import tarfile
 from urllib.request import urlopen
 
 
@@ -13,10 +14,12 @@ class RFCDownloader(object):
     RFC_BULK = "https://www.rfc-editor.org/in-notes/tar/RFC-all.tar.gz"
     STORAGE_PATH = "~/.rfc"
 
-    def download_bulk(self):
+    def update_bulk(self):
         full_path = self._get_storage_path()
         if not os.path.exists(full_path):
             os.mkdir(full_path)
+        self._download_file(self.RFC_BULK, full_path)
+        self._uncompress_bulk_file()
 
     def is_connected(self):
         try:
@@ -26,10 +29,46 @@ class RFCDownloader(object):
             return False
 
     def is_data_present(self):
-        return os.path.exists(self._get_storage_path())
+        path = self._get_storage_path()
+        return os.path.exists(path) and len(os.listdir(path)) > 0
 
     def _get_storage_path(self):
         return os.path.expanduser(self.STORAGE_PATH)
+
+    @staticmethod
+    def _download_file(url, dest):
+        file_name = url.split('/')[-1]
+        u = urlopen(url)
+        f = open(os.path.join(dest, file_name), 'wb')
+        meta = u.info()
+        file_size = 0
+        for header in meta._headers:
+            if header[0] == "Content-Length":
+                file_size = int(header[1])
+                break
+
+        print("Downloading: %s Bytes: %s" % (file_name, file_size))
+
+        file_size_dl = 0
+        block_sz = 8192
+        while True:
+            buffer = u.read(block_sz)
+            if not buffer:
+                break
+
+            file_size_dl += len(buffer)
+            f.write(buffer)
+            status = r"[%3.2f%%]" % (file_size_dl * 100. / file_size)
+            status += chr(8) * (len(status) + 1)
+            print(status)
+        f.close()
+
+    def _uncompress_bulk_file(self):
+        file_name = self.RFC_BULK.split('/')[-1]
+        archive_path = os.path.join(self._get_storage_path(), file_name)
+        with tarfile.open(archive_path) as tar_file:
+            tar_file.extractall(self._get_storage_path())
+        os.remove(archive_path)
 
 
 class RFCSearcher(object):
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import unittest
 
 from rfc.rfc import RFCDownloader
@@ -7,13 +9,22 @@ class TestDownloader(unittest.TestCase):
     def setUp(self):
         super().setUp()
         self.downloader = RFCDownloader()
+        self.remove_data()
+        RFCDownloader.STORAGE_PATH = "/tmp/rfc_python_test/"
+        RFCDownloader.RFC_BULK = "https://www.rfc-editor.org/in-notes/tar/RFCs0001-0500.tar.gz"  # So we don't actually download the whole BULK file
+
+    @staticmethod
+    def remove_data():
+        full_path = os.path.expanduser(RFCDownloader.STORAGE_PATH)
+        if os.path.exists(full_path):
+            shutil.rmtree(full_path)
 
     def test_internet_connection(self):
         self.assertTrue(self.downloader.is_connected(), "No internet connection, cannot continue tests")
 
     def test_data_available(self):
         self.assertFalse(self.downloader.is_data_present(), "Data shouldn't be present on the system at this point")
-        self.downloader.download_bulk()
+        self.downloader.update_bulk()
         self.assertTrue(self.downloader.is_data_present(), "Data should be present")
 
 
